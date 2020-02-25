1c9fc6564332a95a284c724dd6de878c83171923
harsh16sur <34474958+harsh16sur@users.noreply.github.com>
2020-02-25 15:34:02 +0530

    Create Graphs.R

---
+++ Graphs.R
@@ -0,0 +1,35 @@
+prop.table(table(heights$sex)) # To get Proportion
+
+names(data) # gives names of fields
+unique(X) #Gives unique values in a list
+table(x) # Gives Frequency Table
+
+
+
+# Defining a CDF
+a <- seq(min(my_data), max(my_data), length = 100)    # define range of values spanning the dataset
+
+cdf_function <- function(x) {    # computes prob. for a single value
+    mean(my_data <= x)
+}
+cdf_values <- sapply(a, cdf_function)
+plot(a, cdf_values)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
